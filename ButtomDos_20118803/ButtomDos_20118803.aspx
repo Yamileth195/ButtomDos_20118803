d2a38a961946f29eaea679487487bf9870d41d84
MINEDUCYT <MINEDUCYT@DESKTOP-VDTUKI5>
2025-01-29 14:58:17 -0600

    Agregar archivos de proyecto.

---
+++ ButtomDos_20118803/ButtomDos_20118803.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ButtomDos_20118803.aspx.cs" Inherits="ButtomDos_20118803.ButtomDos_20118803" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Font-Bold="true" Text="View Supplier" />
+            <br /><br />
+            <asp:Button runat="server" CommandArgument="Adidas" CommandName="View" 
+                Text="Adidas" />
+            <asp:Button runat="server" CommandArgument="Babolat" CommandName="View" 
+                 Text="Babolat" />
+             <asp:Button runat="server" CommandArgument="Head" CommandName="View" 
+              Text="Head" />
+            <asp:Button runat="server" CommandArgument="Nike" CommandName="View" 
+                Text="Nike" />
+            <asp:Button runat="server" CommandArgument="Prince" CommandName="View" 
+                Text="Prince" />
+            <asp:Button runat="server" CommandArgument="Canccel" CommandName="View" 
+                 Text="Cancel" /><br /><br />
+            <asp:Label runat="server" ID="lblMessage" /><br />
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ ButtomDos_20118803/ButtomDos_20118803.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ButtomDos_20118803
+{
+    public partial class ButtomDos_20118803 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, CommandEventArgs e)
+        {
+            if (e.CommandName == "View")
+
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Aqua;
+                switch (e.CommandArgument.ToString())
+                {
+                    case "Adidas":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Babolat":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Head":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                    case "Nike":
+                        lblMessage.Text = e.CommandArgument.ToString()
+                        ; break;
+                    case "Prince":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                }
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Cancelled";
+            }
+            }
+
+        }
+    }
